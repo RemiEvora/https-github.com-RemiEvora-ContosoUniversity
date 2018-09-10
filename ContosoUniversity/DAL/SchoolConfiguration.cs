033144539f7ed78a2d6a3a23e7a93df043c47188
Revora313 <39279111+Revora313@users.noreply.github.com>
2018-09-10 14:00:21 -0500

    Updated files to Step 6, 'Creating a More Complex Data Model'

---
+++ ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using ContosoUniversity.DAL;                         //Added to remove DbInterception errors 
+using System.Data.Entity.Infrastructure.Interception;// on line 13-14
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
